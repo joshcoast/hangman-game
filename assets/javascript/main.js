1abac44c3d56af67bc3420b05c95e8e0a279059f
Josh Coast <josh@culturefoundry.com>
2018-02-10 17:56:41 -0700

    adds pseudocode

---
+++ assets/javascript/main.js
@@ -0,0 +1,65 @@
+
+// Define wordList[]
+// Define numberOfTurns
+// Define playedLetters[]
+// Define loseRecord
+// Define winRecord
+// Define winningLetters[]
+
+// Push any key to start display -> "Press any key to get started!" -> run gameStart()
+
+// gameStart()
+
+  // Randomly pic a word form an array
+       //wordInPlay = wordList[RANDOM]
+	// display _ for each letter
+			 // find html id and print in the number of letters
+	// separate out word letters into an array
+			// wordLetters[ b, o, b];
+
+// gamePlay() 
+
+	// for (i=0; i < numberOfTurns; i++ )
+	
+		// Get the user input. 
+		   // userKey
+		
+			// Add letter to playedLetters array and display
+			
+			// If letter is already played, display "you already played X, choose a new letter" return to loop. 
+
+			// else if letter matches
+				/*
+				if (wordInPlay.indexOf(userKey) > -1)
+				{
+					// replace that letter in the "_" display
+					// Add 1 to winningLetters var
+				}
+				*/
+				
+			// else letter doesn't match
+
+	  // If word is complete (winningLetters == wordLetters.length), run gameEnds("win")
+
+		// If turns are less than 1, run gameEnds("lose")
+
+// gameEnds(result)
+
+  // Reset NumberOfTurns
+  // Reset playedLetters array 
+
+	// If result === win, 
+		// display you win screen
+		// Add one to win record
+		// start over
+
+	// If result === lose, 
+		// display you lose screen
+		// Add one to loose record 
+
+
+
+
+
+
+
