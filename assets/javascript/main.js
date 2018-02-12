06f2c0a2412dd19ac50211a8a6fe17a70d5145f4
Josh Coast <josh@culturefoundry.com>
2018-02-11 18:09:09 -0700

    add placeholders and letters played

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,65 +1,130 @@
+var wordList = ["eloquent", "javascript", "second", "edition"];
+var wordInPlayLetters = [];
+var numberOfTurns = 5;
+var turnsTaken = 0;
+var playedLetters = [];
+var loseRecord = 0;
+var winRecord = 0;
+var winningLetters = [];
 
-// Define wordList[]
-// Define numberOfTurns
-// Define playedLetters[]
-// Define loseRecord
-// Define winRecord
-// Define winningLetters[]
 
-// Push any key to start display -> "Press any key to get started!" -> run gameStart()
 
-// gameStart()
 
-  // Randomly pic a word form an array
-       //wordInPlay = wordList[RANDOM]
-	// display _ for each letter
-			 // find html id and print in the number of letters
+
+
+
+// Press Any Key To Start
+document.onkeypress = function (event) {
+	// Push any key to start display -> "Press any key to get started!" -> run gameStart()
+	if (document.body.classList.contains("gameintro")) {
+		document.body.classList.replace("gameintro", "gamerunning");
+		//remove intro screen
+		var sel = document.getElementById("intro");
+		sel.remove(1);
+		//gamePlay()
+		setBoard();
+	} else {}
+};
+
+// Setup Board
+function setBoard() {
+	// Randomly pic a word form an array
+	var wordInPlay = wordList[Math.floor(Math.random() * wordList.length)];
+	console.log(wordInPlay);
+	console.log("---------");
+
 	// separate out word letters into an array
-			// wordLetters[ b, o, b];
+	for (var i = 0; i < wordInPlay.length; i++) {
+		console.log(wordInPlay.charAt(i));
+		wordInPlayLetters.push(wordInPlay.charAt(i));
+	}
+	console.log(wordInPlayLetters);
 
-// gamePlay() 
+	for (var i = 0; i < wordInPlayLetters.length; i++) {
+		var listItem = document.createElement("LI");
+		var placeHolder = document.createTextNode("_");
+		listItem.appendChild(placeHolder);
+		document.getElementById("letterTiles").appendChild(listItem);
+	}
+	gamePlay()
+}
+
+function gamePlay() {
+	// Get the user input and process 
+	document.onkeypress = function (event) {
+		userNewPick = event.key;
+		if (playedLetters.length < numberOfTurns) {
+			// Find out if the litter has already been played
+			if (playedLetters.includes(userNewPick)) {
+				//Display message that letter has been played already
+				console.log("this has already been played");
+			}else{
+				//Add letter played to array
+				playedLetters.push(userNewPick);
+				//Add letter to played letters display
+				var listItem = document.createElement("LI");
+				var placeHolder = document.createTextNode(userNewPick);
+				listItem.appendChild(placeHolder);
+				document.getElementById("playedLettersDisplay").appendChild(listItem);
+			}
+			
+			console.log(playedLetters);
+			console.log("----");
+			
+			// Check against Already played letters
+
+			
+
+			// for (var i = 0; i < playedLetters.length; i++){
+			// 	if (playedLetters[i] === event.key) {
+			// 		console.log("yep " + playedLetters[i] + " = " + event.key);
+			// 		console.log("----");
+			// 	}else{
+			// 		console.log("nope");
+			// 		console.log("----");
+			// 	}
+			// }
+		}
+	}
+
 
 	// for (i=0; i < numberOfTurns; i++ )
-	
-		// Get the user input. 
-		   // userKey
-		
-			// Add letter to playedLetters array and display
-			
-			// If letter is already played, display "you already played X, choose a new letter" return to loop. 
 
-			// else if letter matches
-				/*
-				if (wordInPlay.indexOf(userKey) > -1)
-				{
-					// replace that letter in the "_" display
-					// Add 1 to winningLetters var
-				}
-				*/
-				
-			// else letter doesn't match
+	// Get the user input. 
+	// userKey
 
-	  // If word is complete (winningLetters == wordLetters.length), run gameEnds("win")
+	// If letter is already played, display "you already played X, choose a new letter" return to loop. 
 
-		// If turns are less than 1, run gameEnds("lose")
+	// else if letter matches
+	/*
+	if (wordInPlay.indexOf(userKey) > -1)
+	{
+		// replace that letter in the "_" display
+		// Add 1 to winningLetters var
+	}
+	*/
+
+	// else letter doesn't match
+
+	// If word is complete (winningLetters == wordLetters.length), run gameEnds("win")
+
+	// If turns are less than 1, run gameEnds("lose")
+}
+
+function endGame() {
+	console.log("Game Over");
+}
 
 // gameEnds(result)
 
-  // Reset NumberOfTurns
-  // Reset playedLetters array 
+// Reset NumberOfTurns
+// Reset playedLetters array 
 
-	// If result === win, 
-		// display you win screen
-		// Add one to win record
-		// start over
+// If result === win, 
+// display you win screen
+// Add one to win record
+// start over
 
-	// If result === lose, 
-		// display you lose screen
-		// Add one to loose record 
-
-
-
-
-
-
-
+// If result === lose, 
+// display you lose screen
+// Add one to loose record
